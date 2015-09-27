2551fab0c1c354133d179b9b6bfbf3c5dbe703db
gtkafka <gtkafka@gmail.com>
2015-09-27 14:22:51 -0700

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,24 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel("StockHistory"),
+
+  sidebarLayout
+  (
+    sidebarPanel
+    (
+      helpText("Enter a stock symbol and information about it
+               will be gathered from yahoo finance."),
+
+      textInput("symb", "Stock Symbol", "AMG"),
+
+      dateRangeInput("dates",
+        "Date range",
+        start = as.Date(Sys.Date()-30),
+        end = as.character(Sys.Date()))
+    ),
+
+    mainPanel(plotOutput("plot"))
+  )
+)
+)
